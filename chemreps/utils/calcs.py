060de80fd58d3a57a7ed0b0f6580132ad16e082d
KidA3995 <dmm219@pitt.edu>
2020-03-30 23:09:29 -0400

    Updated the dihedral calculation to skip evauation of the sign and take absolute value instead.

--- chemreps/utils/calcs.py
+++ chemreps/utils/calcs.py
@@ -131,17 +131,4 @@ def torsion(molecule, atomi, atomj, atomk, atoml):
     z = abcz * bcdz
     dihedral = (x + y + z) / (sin(ang(a, b, c)) * sin(ang(b, c, d)))
     dihedral = acos(dihedral)
-
-    cross_x = abcy * bcdz - abcz * bcdy
-    cross_y = abcz * bcdx - abcx * bcdz
-    cross_z = abcx * bcdy - abcy * bcdx
-    norm = cross_x * cross_x + cross_y * cross_y + cross_z * cross_z
-    cross_x /= norm
-    cross_y /= norm
-    cross_z /= norm
-    sign = 1.0
-    dot = cross_x * uvec(0, b, c) + cross_y * \
-        uvec(1, b, c) + cross_z * uvec(2, b, c)
-    if dot < 0.0:
-        sign == -1.0
-    return np.float16(sign * dihedral)
+    return np.abs(dihedral)
